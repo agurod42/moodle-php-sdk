c6cf2cb2f38495c975246ba97ee9c0207cbd937a
“Agustín <agurz@icloud.com>
2017-02-23 16:36:18 -0300

    index.php cleaned

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 require_once 'vendor/autoload.php';
-
-use \MoodleSDK\Api\Model\User;
-use \MoodleSDK\Auth\AuthTokenCredential;
-use \MoodleSDK\Rest\RestApiContext;
-
-$context = new RestApiContext('uycls185');
-$context->setCredential(new AuthTokenCredential('34c6dbe3adb6e86efb05a39692ec77d8'));
-$context->setSecureConnection(false);
-
-$user = new User();
-$userList = $user->all($context);
-
-print_r(
-
-    $user
-        ->setUsername('agurodriguez')
-        ->setPassword('Test..01')
-        ->setFirstName('Agustín')
-        ->setLastName('Rodríguez')
-        ->setFullName('Agustín Rodríguez')
-        ->setEmail('agurz@icloud.com')
-        ->create($context)
-
-);
