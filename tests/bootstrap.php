1c4df3686a7ef9ad2a8cad12ec4e176797dd4fdf
“Agustín <agurz@icloud.com>
2017-02-23 23:33:33 -0300

    Changes to make Travis CI build pass

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
-if (class_exists('Dotenv\Dotenv')) {
+if (file_exists(__DIR__.'/../.env')) {
     $dotenv = new Dotenv\Dotenv(__DIR__.'/..');
     $dotenv->load();
 }
